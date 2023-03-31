fba31c121899eeb42bfb39ce07bd906d12dc90ce
mnjqoo <mipo6885@gmail.com>
2023-03-31 12:52:36 +0900

    회원가입 구현

---
+++ Giggle/src/main/java/com/giggler/giggle/dto/UserDTO.java
@@ -0,0 +1,90 @@
+package com.giggler.giggle.dto;
+
+import java.sql.Timestamp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDTO {
+
+	private int user_no;
+	private String user_email;
+	private String user_pwd;
+	private String user_nick;
+	private String user_birth;
+	private Timestamp user_joindate;
+	private String open_yn = "Y";	//기본값 Y 세팅
+	private String status_message;
+	private String prifile_image;
+	private String back_image;
+	public int getUser_no() {
+		return user_no;
+	}
+	public void setUser_no(int user_no) {
+		this.user_no = user_no;
+	}
+	public String getUser_email() {
+		return user_email;
+	}
+	public void setUser_email(String user_email) {
+		this.user_email = user_email;
+	}
+	public String getUser_pwd() {
+		return user_pwd;
+	}
+	public void setUser_pwd(String user_pwd) {
+		this.user_pwd = user_pwd;
+	}
+	public String getUser_nick() {
+		return user_nick;
+	}
+	public void setUser_nick(String user_nick) {
+		this.user_nick = user_nick;
+	}
+	public String getUser_birth() {
+		return user_birth;
+	}
+	public void setUser_birth(String user_birth) {
+		this.user_birth = user_birth;
+	}
+	public Timestamp getUser_joindate() {
+		return user_joindate;
+	}
+	public void setUser_joindate(Timestamp user_joindate) {
+		this.user_joindate = user_joindate;
+	}
+	public String getOpen_yn() {
+		return open_yn;
+	}
+	public void setOpen_yn(String open_yn) {
+		this.open_yn = open_yn;
+	}
+	public String getStatus_message() {
+		return status_message;
+	}
+	public void setStatus_message(String status_message) {
+		this.status_message = status_message;
+	}
+	public String getPrifile_image() {
+		return prifile_image;
+	}
+	public void setPrifile_image(String prifile_image) {
+		this.prifile_image = prifile_image;
+	}
+	public String getBack_image() {
+		return back_image;
+	}
+	public void setBack_image(String back_image) {
+		this.back_image = back_image;
+	}
+	@Override
+	public String toString() {
+		return "MemberDTO [user_no=" + user_no + ", user_email=" + user_email + ", user_pwd=" + user_pwd
+				+ ", user_nick=" + user_nick + ", user_birth=" + user_birth + ", user_joindate=" + user_joindate
+				+ ", open_yn=" + open_yn + ", status_message=" + status_message + ", prifile_image=" + prifile_image
+				+ ", back_image=" + back_image + "]";
+	}
+	
+	
+	
+}
