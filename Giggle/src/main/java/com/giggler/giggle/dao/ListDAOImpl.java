25a8b1f56991f2a2d30fd4815eee0318d229e6ea
kyeunghee <kyeunghj.D@gmail.com>
2023-04-09 16:59:24 +0900

    검색 user 추가

--- Giggle/src/main/java/com/giggler/giggle/dao/ListDAOImpl.java
+++ Giggle/src/main/java/com/giggler/giggle/dao/ListDAOImpl.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import com.giggler.giggle.dto.ListDTO;
+import com.giggler.giggle.dto.UserDTO;
 
 @Repository
 public class ListDAOImpl implements ListDAO {
@@ -33,7 +34,7 @@ private static final Logger logger = LoggerFactory.getLogger(ListDAOImpl.class);
 	//검색 피드 목록 - feed용 VUE 
 	@Override
 	public List<ListDTO> searchfeedList(ListDTO listDTO) throws Exception {
-		logger.info("검색용 DAO 지나감");
+		logger.info("검색용 DAO 지나감- feed용 VUE");
 		return  sqlSession.selectList( Namespace + ".selectsearchfeedList" ,listDTO);
 	}//END - 검색 피드 목록
 	
@@ -43,5 +44,13 @@ private static final Logger logger = LoggerFactory.getLogger(ListDAOImpl.class);
 //		logger.info("검색용 DAO 지나감- user용 VUE");
 //		return  sqlSession.selectList( Namespace + ".selectsearchuserList" ,listDTO);
 //	}//END - 검색 피드 목록
+	
+	//검색 피드 목록 - user용 VUE 	
+	@Override
+	public List<UserDTO> searchuserList(String keyword) throws Exception {
+		System.out.println("검색용 DAO 지나감- user용 VUE");
+		
+		return sqlSession.selectList(Namespace + ".selectsearchuserList", keyword);		
+	}//END - 검색 피드 목록
 
 }// END - public class ListDAOImpl implements ListDAO
