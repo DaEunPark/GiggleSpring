eabae274fc19b3b9dde73716b58053ab0ef0e119
ffrogy <120000570+ffrogy@users.noreply.github.com>
2023-04-14 11:47:15 +0900

    folloing 수정

--- Giggle/src/main/java/com/giggler/giggle/controller/FollowListController.java
+++ Giggle/src/main/java/com/giggler/giggle/controller/FollowListController.java
@@ -7,6 +7,7 @@ import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +24,7 @@ public class FollowListController {
 	private FollowListService followlistService;
 	
 	// 내가 팔로우한 유저 목록
-	@PostMapping("/followingList/{uer_no}")
+	@GetMapping("/followingList/{user_no}")
 	public List<FollowListDTO> followingList(@PathVariable int user_no) throws Exception{
 		
 		logger.info("FollowListController에서 following()실행...");
